229b203c67a7976e35875c1d7c7e388826bd377a
jcada05 <76962892+jcada05@users.noreply.github.com>
2024-06-25 23:08:48 +0800

    Add files via upload

---
+++ ProductListMVC/Models/Product.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ProductListMvc.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
